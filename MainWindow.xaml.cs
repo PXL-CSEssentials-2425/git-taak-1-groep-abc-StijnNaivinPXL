da2f6c93c2e0ba25b473946f9e5390cc019da577
ThisMight <stijn.naivin@student.pxl.be>
2024-09-30 16:27:27 +0200

    Test Commit in mijn repo

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KleurAnalyse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Red.MouseEnter += Red_MouseEnter;
+        }
+
+        private void Red_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = Brushes.Red;
+        }
+    }
+}
