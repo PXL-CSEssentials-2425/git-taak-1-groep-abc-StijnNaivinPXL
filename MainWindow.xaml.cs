cda1a5d97e7dc95254bd4e895e96067723347895
ThisMight <stijn.naivin@student.pxl.be>
2024-10-06 16:09:37 +0200

    KLEUR-01, KLEUR-02, KLEUR-03
    Alle kleuren in een keer megenomen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,15 +17,52 @@ namespace KleurAnalyse
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Dictionary<string, string> Colors = new Dictionary<string, string>()
+        {
+            //Is this optimised? Nahh, but it is worth it.
+            { "Green", "#008000" },
+            { "Yellow", "#FFFF00" },
+            { "Blue", "#0000FF" },
+            { "Red", "#FF0000" }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
             Red.MouseEnter += Red_MouseEnter;
+            Red.MouseLeave += SetColorToGray;
+            Blue.MouseEnter += Blue_MouseEnter;
+            Blue.MouseLeave += SetColorToGray;
+            Yellow.MouseEnter += Yellow_MouseEnter;
+            Yellow.MouseLeave += SetColorToGray;
+            Green.MouseEnter += Green_MouseEnter;
+            Green.MouseLeave += SetColorToGray;
         }
 
         private void Red_MouseEnter(object sender, MouseEventArgs e)
         {
-            this.Background = Brushes.Red;
+            BackgroundGrid.Background = Brushes.Red;
+        }
+
+        private void Blue_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BackgroundGrid.Background = Brushes.Blue;
+        }
+
+        private void Green_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BackgroundGrid.Background = Brushes.Green;
+        }
+
+        private void Yellow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BackgroundGrid.Background = Brushes.Green;
+        }
+
+        private void SetColorToGray(object sender, RoutedEventArgs e)
+        {
+            BackgroundGrid.Background = Brushes.Gray;
         }
     }
 }
